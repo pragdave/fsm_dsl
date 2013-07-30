fa2577f7a59582b96a3ac7fdb91bd48370fda047
pragdave <dave@pragprog.com>
2013-07-29 21:26:23 -0500

    update tests

--- mix.exs
+++ mix.exs
@@ -9,14 +9,13 @@ defmodule FsmDsl.Mixfile do
     ]
   end
 
-  # Configuration for the OTP application
   def application do
     []
   end
 
-  # Returns the list of dependencies in the format:
-  # { :foobar, "~> 0.1", git: "https://github.com/elixir-lang/foobar.git" }
   defp deps do
-    []
+    [ 
+       {:meck,  github: "eproxus/meck" }
+    ]
   end
 end
